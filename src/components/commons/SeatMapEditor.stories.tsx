59e92a221f4d76a9a4193b041a155617f25028e3
st4l1nR <stalinramosbw@gmail.com>
2023-04-25 21:44:19 -0500

    add:ListEventSections

--- src/components/commons/SeatMapEditor.stories.tsx
+++ src/components/commons/SeatMapEditor.stories.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Meta, StoryFn } from '@storybook/react';
 import SeatMapEditor, { props } from './SeatMapEditor';
-
+import { useForm } from 'react-hook-form';
 export default {
   title: 'Molecules/SeatMapEditor',
   component: SeatMapEditor,
 } as Meta;
 
-const Template: StoryFn<props> = (args) => <SeatMapEditor {...args} />;
+const Template: StoryFn<props> = (args) => {
+  const { control } = useForm();
+  return <SeatMapEditor name="spots" control={control} {...args} />;
+};
 
 export const Default = Template.bind({});
 Default.args = {
